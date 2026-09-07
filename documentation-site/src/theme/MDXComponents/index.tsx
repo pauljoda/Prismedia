05acbc73f2ef79dc0a16082c323f644b5a4fe496
Paul Davis <pauldavis101@gmail.com>
2026-09-06 20:04:00 -0500

    fix(docs): correct table layout and soften reading surfaces

--- documentation-site/src/theme/MDXComponents/index.tsx
+++ documentation-site/src/theme/MDXComponents/index.tsx
@@ -1,4 +1,5 @@
 import MDXComponents from '@theme-original/MDXComponents';
 import DocScreenshot, {DocScreenshotGrid} from '@site/src/components/docs/DocScreenshot';
+import DocTable from '@site/src/components/docs/DocTable';
 
-export default {...MDXComponents, DocScreenshot, DocScreenshotGrid};
+export default {...MDXComponents, table: DocTable, DocScreenshot, DocScreenshotGrid};
